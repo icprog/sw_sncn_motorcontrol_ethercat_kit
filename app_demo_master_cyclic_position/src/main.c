cddc59b32c278c0bd3542e0ca0261439eafc2ea2
Slava <vtretyakov@synapticon.com>
2014-06-29 05:05:25 +0200

    Simplfy and style demo application

--- app_demo_master_cyclic_position/src/main.c
+++ app_demo_master_cyclic_position/src/main.c
@@ -1,4 +1,3 @@
-
 /**
  * @file main.c
  * @brief Example Master App for Cyclic Synchronous Position (on PC)
@@ -8,7 +7,6 @@
 
 #include <ctrlproto_m.h>
 #include <ecrt.h>
-#include "ethercat_setup.h"
 #include <stdio.h>
 #include <stdbool.h>
 #include <profile.h>
@@ -16,22 +14,37 @@
 #include <motor_define.h>
 #include <sys/time.h>
 #include <time.h>
+#include "ethercat_setup.h"
 
+void find_initial_position(){
+    for (int i = 1; i < n_init_ticks + 1; i++){
+        set_position_ticks(i, slave_number, slv_handles);
+        actual_position = get_position_actual_ticks(slave_number,
+                                slv_handles);
+        printf("    Position: %d\n\n", actual_position);
+    }
+    for (int j = n_init_ticks + 1; j > -1; j--){
+        set_position_ticks(j, slave_number, slv_handles);
+        actual_position = get_position_actual_ticks(slave_number,
+                                slv_handles);
+        printf("    Position: %d\n\n", actual_position);
+    }
+}
 
-int main()
-{
-    int flag = 0;
+int main() {
 
-    int acceleration = 350;             // rpm/s
-    int deceleration = 350;             // rpm/s
-    int velocity = 350;                 // rpm
-    int actual_position = 0;            // ticks
-    int target_position = 0;            // ticks
-    int actual_velocity = 0;            // rpm
-    float actual_torque;                // mNm
+    int final_target_position = 12288; // ticks
+    int acceleration = 600; // rpm/s
+    int deceleration = 600; // rpm/s
+    int velocity = 350; // rpm
+    int actual_position = 0; // ticks
+    int target_position = 0; // ticks
+    int actual_velocity = 0; // rpm
+    float actual_torque; // mNm
     int steps = 0;
-    int i = 1;
     int position_ramp = 0;
+    int direction = 1;
+    int n_init_ticks = 3;
 
     int slave_number = 0;
 
@@ -44,77 +57,79 @@ int main()
     /* Initialize all connected nodes with Mandatory Motor Configurations (specified in motor_config)*/
     init_nodes(&master_setup, slv_handles, TOTAL_NUM_OF_SLAVES);
 
-    /* Initialize the node specified with slave_number with CSP configurations (specified under motor_config)*/
-    set_operation_mode(CSP, slave_number, &master_setup, slv_handles, TOTAL_NUM_OF_SLAVES);
+    /* Initialize the node specified with slave_number with CSP configurations (specified in motor_config)*/
+    set_operation_mode(CSP, slave_number, &master_setup, slv_handles,
+            TOTAL_NUM_OF_SLAVES);
 
     /* Enable operation of node in CSP mode */
-    enable_operation(slave_number, &master_setup, slv_handles, TOTAL_NUM_OF_SLAVES);
+    enable_operation(slave_number, &master_setup, slv_handles,
+            TOTAL_NUM_OF_SLAVES);
 
     /* Initialize position profile parameters */
     initialize_position_profile_limits(slave_number, slv_handles);
 
+    find_position();
 
-    i = 0;
-    while(1)
-    {
-        /* Update the process data (EtherCat packets) sent/received from the node */
-        pdo_handle_ecat(&master_setup, slv_handles, TOTAL_NUM_OF_SLAVES);
+    for (int times = 0; times < 10; times++) {
 
-        if(master_setup.op_flag)    /*Check if the master is active*/
-        {
-            /* Read Actual Position from the node for initialization */
-            if(flag == 0)
+        /* Compute a target position */
+        target_position = actual_position + direction
+                * final_target_position;//
+
+        /* Compute steps needed for the target position */
+        steps = init_position_profile_params(target_position,
+                actual_position, velocity, acceleration,
+                deceleration, slave_number, slv_handles);
+
+        for (int i = 1; i < steps + 1; i++) {
+
+            /* Update the process data (EtherCat packets) sent/received from the node */
+            pdo_handle_ecat(&master_setup, slv_handles, TOTAL_NUM_OF_SLAVES);
+
+            if (master_setup.op_flag) /*Check if the master is active*/
             {
-                 actual_position = get_position_actual_ticks(slave_number, slv_handles);
-                 i = i+1;
-                 if(i>3)
-                 {
-                     /* Compute a target position */
-                     target_position =  actual_position + 12288;//
 
-                     /* Compute steps needed for the target position */
-                     steps = init_position_profile_params(target_position, actual_position, velocity, acceleration, 
-                                deceleration, slave_number, slv_handles);
-                     flag = 1;
-                     i = 1;
-                     printf("steps %d target %d actual %d\n", steps, target_position, actual_position);
-                 }
-            }
-
-            if(i<steps && flag == 1)
-            {
                 /* Generate target position steps */
-                position_ramp =  generate_profile_position(i, slave_number, slv_handles);
-                //printf(" position_ramp %d\n", position_ramp);
+                position_ramp = generate_profile_position(i, slave_number,
+                        slv_handles);
+
                 /* Send target position for the node specified by slave_number */
                 set_position_ticks(position_ramp, slave_number, slv_handles);
-                i = i+1;
-            }
-            if(i >= steps && flag == 1)
-            {
-                break;
-            }
 
-            /* Read actual node sensor values */
-            actual_position = get_position_actual_ticks(slave_number, slv_handles);
-            actual_velocity = get_velocity_actual_rpm(slave_number, slv_handles);
-            actual_torque = get_torque_actual_mNm(slave_number, slv_handles);
-            printf("actual position %d actual velocity %d actual_torque %f\n", actual_position, actual_velocity, actual_torque);
+                /* Read actual node sensor values */
+                actual_position = get_position_actual_ticks(slave_number,
+                        slv_handles);
+                actual_velocity = get_velocity_actual_rpm(slave_number,
+                        slv_handles);
+                actual_torque
+                        = get_torque_actual_mNm(slave_number, slv_handles);
+
+                printf("\033[2J\033[1;1H\n\n"); //clears the terminal
+                printf("    Position: %d\n\n", actual_position);
+                printf("    Velocity: %d\n\n", actual_velocity);
+                printf("    Torque: %f\n\n", actual_torque);
+            }
         }
+        direction = direction * -1;
     }
 
     /* Quick stop position mode (for emergency) */
-    quick_stop_position(slave_number, &master_setup, slv_handles, TOTAL_NUM_OF_SLAVES);
+    quick_stop_position(slave_number, &master_setup, slv_handles,
+            TOTAL_NUM_OF_SLAVES);
 
     /* Regain control of node to continue after quick stop */
-    renable_ctrl_quick_stop(CSP, slave_number, &master_setup, slv_handles, TOTAL_NUM_OF_SLAVES); //after quick-stop
+    renable_ctrl_quick_stop(CSP, slave_number, &master_setup, slv_handles,
+            TOTAL_NUM_OF_SLAVES); //after quick-stop
 
-    set_operation_mode(CSP, slave_number, &master_setup, slv_handles, TOTAL_NUM_OF_SLAVES);
+    set_operation_mode(CSP, slave_number, &master_setup, slv_handles,
+            TOTAL_NUM_OF_SLAVES);
 
-    enable_operation(slave_number, &master_setup, slv_handles, TOTAL_NUM_OF_SLAVES);
+    enable_operation(slave_number, &master_setup, slv_handles,
+            TOTAL_NUM_OF_SLAVES);
 
     /* Shutdown node operations */
-    shutdown_operation(CSP, slave_number, &master_setup, slv_handles, TOTAL_NUM_OF_SLAVES);
+    shutdown_operation(CSP, slave_number, &master_setup, slv_handles,
+            TOTAL_NUM_OF_SLAVES);
 
     return 0;
 }
--- app_demo_master_cyclic_velocity/src/main.c
+++ app_demo_master_cyclic_velocity/src/main.c
@@ -1,4 +1,3 @@
-
 /**
  * @file main.c
  * @brief Example Master App for Cyclic Synchronous Velocity (on PC)
@@ -8,7 +7,6 @@
 
 #include <ctrlproto_m.h>
 #include <ecrt.h>
-#include "ethercat_setup.h"
 #include <stdio.h>
 #include <stdbool.h>
 #include <profile.h>
@@ -16,89 +14,90 @@
 #include <motor_define.h>
 #include <sys/time.h>
 #include <time.h>
+#include "ethercat_setup.h"
 
+int main() {
 
-int main()
-{
-	int flag = 0;
+    int final_target_velocity = -2000; //rpm
+    int acceleration = 100; //rpm/s
+    int deceleration = 100; //rpm/s
+    int steps = 0;
+    int target_velocity = 0; // rpm
+    int actual_velocity = 0; // rpm
+    int actual_position; // ticks
+    float actual_torque; // mNm
+    int sdo_update = 1; // 1- yes / 0 - no
+    int slave_number = 0;
 
-	int final_target_velocity = -2000;			//rpm
-	int acceleration= 100;						//rpm/s
-	int deceleration = 100;					//rpm/s
-	int steps = 0;
-	int i = 1;
-	int target_velocity = 0;					// rpm
-	int actual_velocity = 0;					// rpm
-	int actual_position;						// ticks
-	float actual_torque;						// mNm
-	int sdo_update = 1;                         // 1- yes / 0 - no
-	int slave_number = 0;
+    /* Initialize Ethercat Master */
+    init_master(&master_setup, slv_handles, TOTAL_NUM_OF_SLAVES);
 
-	/* Initialize Ethercat Master */
-	init_master(&master_setup, slv_handles, TOTAL_NUM_OF_SLAVES);
+    /* Initialize torque parameters */
+    initialize_torque(slave_number, slv_handles);
 
-	/* Initialize torque parameters */
-	initialize_torque(slave_number, slv_handles);
+    /* Initialize all connected nodes with Mandatory Motor Configurations (specified in config/motor/)*/
+    init_nodes(&master_setup, slv_handles, TOTAL_NUM_OF_SLAVES);
 
-	/* Initialize all connected nodes with Mandatory Motor Configurations (specified under config/motor/)*/
-	init_nodes(&master_setup, slv_handles, TOTAL_NUM_OF_SLAVES);
+    /* Initialize the node specified with slave_number with CSV configurations (specified in config/motor/)*/
+    set_operation_mode(CSV, slave_number, &master_setup, slv_handles,
+            TOTAL_NUM_OF_SLAVES);
 
-	/* Initialize the node specified with slave_number with CSV configurations (specified under config/motor/)*/
-	set_operation_mode(CSV, slave_number, &master_setup, slv_handles, TOTAL_NUM_OF_SLAVES);
+    /* Enable operation of node in CSV mode */
+    enable_operation(slave_number, &master_setup, slv_handles,
+            TOTAL_NUM_OF_SLAVES);
 
-	/* Enable operation of node in CSV mode */
-	enable_operation(slave_number, &master_setup, slv_handles, TOTAL_NUM_OF_SLAVES);
+    /* Initialize velocity profile parameters */
+    steps = init_velocity_profile_params(final_target_velocity,
+            actual_velocity, acceleration, deceleration, slave_number,
+            slv_handles);
 
-	i = 1;
-	/* Initialize velocity profile parameters */
-	steps = init_velocity_profile_params(final_target_velocity, actual_velocity, acceleration, 
-				deceleration, slave_number, slv_handles);
+    for (int i = 1; i < steps + 1; i++) {
+        /* Update the process data (EtherCat packets) sent/received from the node */
+        pdo_handle_ecat(&master_setup, slv_handles, TOTAL_NUM_OF_SLAVES);
 
+        if (master_setup.op_flag) /*Check if the master is active*/
+        {
+            if (i < steps) {
+                /* Generate target velocity steps */
+                target_velocity = generate_profile_velocity(i, slave_number,
+                        slv_handles);
 
-	while(1)
-	{
-		/* Update the process data (EtherCat packets) sent/received from the node */
-		pdo_handle_ecat(&master_setup, slv_handles, TOTAL_NUM_OF_SLAVES);
+                /* Send target velocity for the node specified by slave_number */
+                set_velocity_rpm(target_velocity, slave_number, slv_handles);
 
-		if(master_setup.op_flag) /*Check if the master is active*/
-		{
-			if(i<steps)
-			{
-				/* Generate target velocity steps */
-				target_velocity = generate_profile_velocity( i, slave_number, slv_handles);
+                /* Read actual node sensor values */
+                actual_velocity = get_velocity_actual_rpm(slave_number,
+                        slv_handles);
+                actual_position = get_position_actual_ticks(slave_number,
+                        slv_handles);
+                actual_torque
+                        = get_torque_actual_mNm(slave_number, slv_handles);
+                printf("\033[2J\033[1;1H\n\n"); //clears the terminal
+                printf("    Velocity: %d\n\n", actual_velocity);
+                printf("    Position: %d\n\n", actual_position);
+                printf("    Torque: %f\n\n", actual_torque);
+            }
+        }
+    }
 
-				/* Send target velocity for the node specified by slave_number */
-				set_velocity_rpm(target_velocity, slave_number, slv_handles);
+    /* Quick stop velocity mode (for emergency) */
+    quick_stop_velocity(slave_number, &master_setup, slv_handles,
+            TOTAL_NUM_OF_SLAVES);
 
-				/* Read actual node sensor values */
-				actual_velocity = get_velocity_actual_rpm(slave_number, slv_handles);
-				actual_position = get_position_actual_ticks(slave_number, slv_handles);
-				actual_torque = get_torque_actual_mNm(slave_number, slv_handles);
-				printf("Velocity: %d Position: %d Torque: %f\n", actual_velocity, actual_position, actual_torque);
-				i = i+1;
-			}
+    /* Regain control of node to continue after quick stop */
+    renable_ctrl_quick_stop(CSV, slave_number, &master_setup, slv_handles,
+            TOTAL_NUM_OF_SLAVES);
 
-			if(i >= steps )
-			{
-				break;
-			}
-		}
-	}
+    set_operation_mode(CSV, slave_number, &master_setup, slv_handles,
+            TOTAL_NUM_OF_SLAVES);
 
-	/* Quick stop velocity mode (for emergency) */
-	quick_stop_velocity(slave_number, &master_setup, slv_handles, TOTAL_NUM_OF_SLAVES);
+    enable_operation(slave_number, &master_setup, slv_handles,
+            TOTAL_NUM_OF_SLAVES);
 
-	/* Regain control of node to continue after quick stop */
-	renable_ctrl_quick_stop(CSV, slave_number, &master_setup, slv_handles, TOTAL_NUM_OF_SLAVES);
+    /* Shutdown node operations */
+    shutdown_operation(CSV, slave_number, &master_setup, slv_handles,
+            TOTAL_NUM_OF_SLAVES);
 
-	set_operation_mode(CSV, slave_number, &master_setup, slv_handles, TOTAL_NUM_OF_SLAVES);
-
-	enable_operation(slave_number, &master_setup, slv_handles, TOTAL_NUM_OF_SLAVES);
-
-	/* Shutdown node operations */
-	shutdown_operation(CSV, slave_number, &master_setup, slv_handles, TOTAL_NUM_OF_SLAVES);
-
-	return 0;
+    return 0;
 }
-
 
